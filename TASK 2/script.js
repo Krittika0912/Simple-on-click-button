3333527f7ae730b2f8ed572ad6dae47c128989d2
Krittika <117507735+Krittika0912@users.noreply.github.com>
2023-09-03 16:40:55 +0530

    Add files via upload

---
+++ TASK 2/script.js
@@ -0,0 +1,8 @@
+function myButton() {
+    var x = document.getElementById("text");
+    if (x.innerHTML === "Hello World !!!") {
+      x.innerHTML = "Krittika here :)";
+    } else {
+      x.innerHTML = "Hello World !!!";
+    }
+  }
